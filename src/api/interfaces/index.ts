b79a10cc7354a66b98cf256d0dc8bb53683df0e4
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-28 07:32:10 +0530

    Add public-samples/mint-financial-mgmt-app-qw9x4b/src/api/interfaces/index.ts

---
+++ src/api/interfaces/index.ts
@@ -0,0 +1,16 @@
+// This file serves as the main entry point for exporting all interface definitions used in the API layer of the Mint Replica application.
+
+// TODO: Implement RequestInterface in ./request.interface.ts
+export { RequestInterface } from './request.interface';
+
+// TODO: Implement ResponseInterface in ./response.interface.ts
+export { ResponseInterface } from './response.interface';
+
+/**
+ * RequestInterface: Interface defining the structure of API request objects
+ * ResponseInterface: Interface defining the structure of API response objects
+ */
+
+// List of pending human tasks:
+// 1. Create and implement the request.interface.ts file (Required)
+// 2. Create and implement the response.interface.ts file (Required)
