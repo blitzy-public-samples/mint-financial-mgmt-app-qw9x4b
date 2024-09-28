699ca1562a29ab836a4f5451a65df34902b019f6
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-28 07:41:33 +0530

    Add public-samples/mint-financial-mgmt-app-qw9x4b/src/mobile/babel.config.js

---
+++ src/mobile/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [],
+};
+
+// Human tasks:
+// TODO: Review and update Babel configuration if needed for any specific project requirements
